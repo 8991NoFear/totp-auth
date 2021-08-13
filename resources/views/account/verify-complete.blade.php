261a1a2b347fc53c2060c37af5c6bcb4bc168749
8991NoFear <binh.ld172971@sis.hust.edu.vn>
2021-08-13 14:28:51 +0700

    update verification ui

--- resources/views/account/verify-complete.blade.php
+++ resources/views/account/verify-complete.blade.php
@@ -10,12 +10,20 @@
     </style>
 </head>
 <body>
-     <div class="d-flex justify-content-center align-items-center flex-column vh-100">
-            <img src="{{ asset('/default-images/task_alt_black_48dp.svg') }}" width="5%" alt="tick" srcset="" style="display: block;">
-            <h2>Thank you</h2>
-            <p>You have verified your account</p>
-            <a href="{{ route('auth.login.index') }}" class="btn btn-primary">continue</a>
+    <div class="row m-0">
+        <div class="col-md-6 offset-md-3">
+            <div class="d-flex justify-content-center align-items-center flex-column vh-100 text-center">
+                <img src="{{ asset('/default-images/task_alt_black_48dp.svg') }}" width="5%" alt="tick" srcset="">
+                <h2>Thank you</h2>
+                <p class="fs-4">You have verified your account</p>
+                <p>Our website support Google Two Factor Authentication (Google 2FA), would you like to enable it now? Google 2FA will improve your account security but not require.</p>
+                <div class="w-100 flex-row justify-content-around">
+                    <a href="{{ route('auth.login.index') }}" class="btn btn-outline-secondary">NO, maybe later</a>
+                    <a href="#" class="btn btn-primary">YES, set up it now</a>
+                </div>
+            </div>
         </div>
+    </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </html>
