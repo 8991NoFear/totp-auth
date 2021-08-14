085b3b30c839bb3a49c7dff9d770ac45ea280d3b
8991NoFear <binh.ld172971@sis.hust.edu.vn>
2021-08-14 15:10:33 +0700

    add logout function

--- resources/views/account/layouts/app.blade.php
+++ resources/views/account/layouts/app.blade.php
@@ -87,13 +87,13 @@
                             <strong>BinhLD</strong>
                         </a>
                         <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2" style="">
-                            <li><a class="dropdown-item" href="route('account.dashboard.index')">Home</a></li>
+                            <li><a class="dropdown-item" href="{{ route('account.dashboard.index') }}">Home</a></li>
                             <li><a class="dropdown-item" href="#">Profile</a></li>
                             <li><a class="dropdown-item" href="{{ route('account.security.index') }}">Security</a></li>
                             <li>
                                 <hr class="dropdown-divider">
                             </li>
-                            <li><a class="dropdown-item" href="#">Logout</a></li>
+                            <li><button type="button" class="dropdown-item" data-bs-toggle="modal" data-bs-target="#exampleModal">Logout</button></li>
                         </ul>
                     </div>
                 </div>
@@ -103,6 +103,27 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                Are you sure logging out?
+            </div>
+            <div class="modal-footer">
+                <form action="{{ route('auth.logout') }}" method="post">
+                    @CSRF
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
+                    <button type="submit" class="btn btn-primary">Yes</button>
+                </form>
+            </div>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
