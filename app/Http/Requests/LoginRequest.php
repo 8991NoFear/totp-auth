51388ff0489c034cc7a56782844263bafda08d30
8991NoFear <binh.ld172971@sis.hust.edu.vn>
2021-08-26 18:28:20 +0700

    add form request

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use App\Models\User;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Validator;
 
 class LoginRequest extends FormRequest
 {
@@ -28,5 +30,26 @@ class LoginRequest extends FormRequest
             'password' => ['required', 'string'],
             'remember_me' => ['nullable', 'string'],
         ];
+    }
+
+    /**
+     * Configure the validator instance.
+     *
+     * @param  Validator  $validator
+     * @return void
+     */
+    public function withValidator(Validator $validator)
+    {
+        $validator->after(function ($validator) {
+            $email = request('email');
+            $user = User::where('email', $email)->first();
+            if ($user != null) {
+                if ($user->email_verified_at == null) {
+                    $validator->errors()->add('email', 'Email has not verified yet');
+                }
+            } else {
+                $validator->errors()->add('email', 'Account doesn\' exists');
+            }
+        });
     }
 }
